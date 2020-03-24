e3cdcb3bb1a4546733004c169ff2d471ea5c385b
Ruud Schiphorst <ruud.schiphorst2@politie.nl>
2020-03-24 12:46:21 +0100

    Eerst checken of username/password kloppen voordat we opslaan

--- app/src/main/java/nl/politie/predev/android/zakboek/CredentialsActivity.java
+++ app/src/main/java/nl/politie/predev/android/zakboek/CredentialsActivity.java
@@ -2,14 +2,25 @@ package nl.politie.predev.android.zakboek;
 
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.os.PersistableBundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 public class CredentialsActivity extends AppCompatActivity {
 
@@ -37,14 +48,60 @@ public class CredentialsActivity extends AppCompatActivity {
 		ok.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View view) {
-				SharedPreferences.Editor editor = settings.edit();
-				editor.putString(PREFS_USERNAME,username.getText().toString());
-				editor.putString(PREFS_PASS,password.getText().toString());
-				editor.commit();
-				finish();
+				testAndFinish();
 			}
 		});
 
+	}
+
+	private void testAndFinish() {
+
+		final String enteredUsername = username.getText().toString();
+		final String enteredPassword = password.getText().toString();
+
+		String json = "{\"username\":\"" + enteredUsername + "\", \"password\":\"" + enteredPassword + "\"}";
+
+		RequestBody body = RequestBody.create(
+				MediaType.parse("application/json"), json);
+
+		Request request = new Request.Builder()
+				.url("https://stempolextras.westeurope.cloudapp.azure.com:8085/api/auth/generatetoken")
+				.post(body)
+				.build();
+
+		OkHttpClient client =  new OkHttpClient();  //getUnsafeOkHttpClient();
+
+		client.newCall(request).enqueue(new Callback() {
+			@Override
+			public void onFailure(Call call, IOException e) {
+				Log.e("err", e.getMessage());
+			}
+
+			@Override
+			public void onResponse(Call call, Response response) throws IOException {
+				if (response.code() == 200) {
+					SharedPreferences.Editor editor = settings.edit();
+					editor.putString(PREFS_USERNAME, enteredUsername);
+					editor.putString(PREFS_PASS,enteredPassword);
+					editor.commit();
+					finish();
+				}else{
+					Handler mainHandler = new Handler(getBaseContext().getMainLooper());
+					Runnable runnable = new Runnable() {
+						@Override
+						public void run() {
+							wrongUsernamePassword();
+						}
+					};
+					mainHandler.post(runnable);
+
+				}
+			}
+		});
+	}
+
+	private void wrongUsernamePassword() {
+		Toast.makeText(this,"Verkeerde username en/of password. Probeer het opnieuw.",Toast.LENGTH_LONG).show();
 	}
 
 	@Override
